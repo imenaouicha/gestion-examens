02c384763b3eedf8526bbd868bd793a54c97ba6f
imenaouicha <imenaouicha226@gmail.com>
2026-01-10 18:52:48 +0100

    Refactor get_connection to use environment variables
    Updated database connection to use environment variables for configuration.

--- db.py
+++ db.py
@@ -1,10 +1,12 @@
 import psycopg2
+import os
 
 def get_connection():
     return psycopg2.connect(
-        dbname="exams_db",
-        user="postgres",
-        password="imen",     # 🔴 mets ton vrai mot de passe
-        host="localhost",
+        host=os.environ.get("DB_HOST", "db.pgnlawlfykbmrivtothz.supabase.co"),
+        database=os.environ.get("DB_NAME", "postgres"),
+        user=os.environ.get("DB_USER", "postgres"),
+        password=os.environ.get("DB_PASS", "imanebdd2003"),
         port="5432"
     )
+
