ae79fa69d50ead2bd09346ad29423fbdd4a4067e
imenaouicha <imenaouicha226@gmail.com>
2026-01-10 23:22:53 +0100

    Refactor get_connection to use bracket notation
    Updated get_connection function to use bracket notation for environment variables.

--- db.py
+++ db.py
@@ -3,11 +3,11 @@ import os
 
 def get_connection():
     return psycopg2.connect(
-        host=os.environ.get("DB_HOST"),
-        database=os.environ.get("DB_NAME"),
-        user=os.environ.get("DB_USER"),
-        password=os.environ.get("DB_PASSWORD"),
+        host=os.environ["DB_HOST"],
+        database=os.environ["DB_NAME"],
+        user=os.environ["DB_USER"],
+        password=os.environ["DB_PASSWORD"],
         port=os.environ.get("DB_PORT", "5432"),
-        sslmode="require"
+        sslmode="require"  # OBLIGATOIRE pour Supabase
     )
 
