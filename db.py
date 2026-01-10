4ae3f31c3796170b4503487af1915851c646454f
imenaouicha <imenaouicha226@gmail.com>
2026-01-10 23:04:01 +0100

    Refactor get_connection to use environment variables
    Updated database connection parameters to use environment variables for password and port, and added SSL mode requirement.

--- db.py
+++ db.py
@@ -3,10 +3,11 @@ import os
 
 def get_connection():
     return psycopg2.connect(
-        host=os.environ.get("DB_HOST", "db.pgnlawlfykbmrivtothz.supabase.co"),
-        database=os.environ.get("DB_NAME", "postgres"),
-        user=os.environ.get("DB_USER", "postgres"),
-        password=os.environ.get("DB_PASS", "imanebdd2003"),
-        port="5432"
+        host=os.environ.get("DB_HOST"),
+        database=os.environ.get("DB_NAME"),
+        user=os.environ.get("DB_USER"),
+        password=os.environ.get("DB_PASSWORD"),
+        port=os.environ.get("DB_PORT", "5432"),
+        sslmode="require"
     )
 
